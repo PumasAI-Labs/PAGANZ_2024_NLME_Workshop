d44114c3dd17e342b1fe22bbb1f3f32a636bc63a
mohamed@pumas.ai <mohamed@pumas.ai>
2024-02-09 15:39:03 +0000

    dose optimisation

--- warfarin/warfarin.jl
+++ warfarin/warfarin.jl
@@ -29,6 +29,7 @@ df2 = unstack(
     :DV,
 )
 df2
+print(names(df2))
 rename!(df2, :DV1 => :conc, :DV2 => :pca)
 
 ## Reading data ##
@@ -92,6 +93,7 @@ model = @model begin
         tover = pop_tover * exp(pd_η[4])
         kout = log(2) / tover
         rin = e0 * kout
+        time = t
     end
 
     @dosecontrol begin
@@ -332,7 +334,7 @@ model3 = @model begin
             lag_ω,
         )
         pd_η ~ MvLogNormal(
-            log.([pop_e0, pop_emax, pop_c50, pop_tover]),
+            log.([pop_e0, 1.0, pop_c50, pop_tover]),
             pd_Ω,
         )
     end
@@ -347,7 +349,7 @@ model3 = @model begin
         Ka = log(2) / tabs
         # PD
         e0 = pd_η[1]
-        emax = pd_η[2]
+        emax = pop_emax * pd_η[2]
         c50 = pd_η[3]
         tover = pd_η[4]
         kout = log(2) / tover
@@ -394,7 +396,7 @@ bts_inf = infer(fpm, Bootstrap(samples = 20))
 sims1 = simobs(model, pop, coef(fpm))
 
 # Simulate 100 populations
-[simobs(model, pop, coef(fpm)) for _ in 1:100]
+sims2 = [simobs(model, pop, coef(fpm)) for _ in 1:100]
 
 # Small time step simulation
 fine_sims = simobs(model, pop, coef(fpm), obstimes = 0.0:0.5:150.0)
@@ -405,12 +407,12 @@ sim_plot(fine_sims, observations = [:conc])
 sim_plot(fine_sims, observations = [:pca])
 
 # Use the empirical Bayes estimates
-sims2 = simobs(model, pop, coef(fpm), empirical_bayes(fpm))
+sims3 = simobs(model, pop, coef(fpm), empirical_bayes(fpm))
 # or
-sims2 = simobs(fpm)
+sims3 = simobs(fpm)
 
 # Fix the random number generator
-rng = default_rng()
+rng = Pumas.default_rng()
 Random.seed!(rng, 123)
 simobs(fpm; rng = rng)
 # or
@@ -418,19 +420,19 @@ simobs(fpm; rng)
 
 # Sample population parameters from the bootstrap samples first
 # Then sample random effects from their priors
-sims3 = simobs(bts_inf, pop, samples = 10)
+sims4 = simobs(bts_inf, pop, samples = 10)
 # or
-@time sims3 = [simobs(bts_inf, pop, samples = 1)[1] for _ in 1:1000]
+@time sims4 = [simobs(bts_inf, pop, samples = 1)[1] for _ in 1:1000]
 
 # VPC from simulations
-vpc_res_conc = vpc(sims3; observations = [:conc])
+vpc_res_conc = vpc(sims4; observations = [:conc])
 vpc_plot(
     vpc_res_conc;
     simquantile_medians = true,
     observations = false,
 )
 
-vpc_res_pca = vpc(sims3; observations = [:pca])
+vpc_res_pca = vpc(sims4; observations = [:pca])
 vpc_plot(
     vpc_res_pca;
     simquantile_medians = true,
@@ -444,18 +446,184 @@ ipreds_df = DataFrame(ipreds)
 print(names(ipreds_df))
 ipreds_df[:,[:id, :time, :conc_ipred, :pca_ipred]]
 
-## Post-processing, AUC and Cmax ##
-
 ## Bayesian workflow ##
 # Reference: https://arxiv.org/abs/2304.04752
 
-bayes_res = fit(
-    model,
+bayes_model = @model begin
+    @param begin
+        # PK parameters
+        pop_CL   ~ LogNormal(log(0.134), 1.0) # L/h/70kg
+        pop_V    ~ LogNormal(log(8.11), 1.0)  # L/70kg
+        pop_tabs ~ LogNormal(log(0.523), 1.0) # h
+        # PD parameters
+        pop_e0   ~ LogNormal(log(100.0), 1.0)
+        pop_emax ~ Normal(-1.0, 1.0)
+        pop_c50  ~ LogNormal(log(1.0), 1.0)
+        pop_tover ~ LogNormal(log(14.0), 1.0)
+        # Inter-individual variability
+        pk_Ω     ~ Constrained(MvNormal([0.01, 0.01, 0.01], 1.0), lower = 0.0) # unitless
+        pd_Ω     ~ Constrained(MvNormal([0.01, 0.01, 0.01, 0.01], 1.0), lower = 0.0) # unitless
+        # Residual variability
+        σ_prop   ~ Constrained(Normal(0.00752, 0.1), lower = 0.0) # unitless
+        σ_add    ~ Constrained(Normal(0.0661, 1.0), lower = 0.0) # mg/L
+        σ_fx     ~ Constrained(Normal(0.01, 1.0), lower = 0.0) # unitless
+    end
+
+    @random begin
+        # mean = 0, covariance = pk_Ω
+        pk_η ~ MvNormal(Diagonal(pk_Ω))
+        # mean = 0, covariance = pd_Ω
+        pd_η ~ MvNormal(Diagonal(pd_Ω))
+    end
+
+    @covariates FSZV FSZCL
+
+    @pre begin
+        # PK
+        CL = FSZCL * pop_CL * exp(pk_η[1])
+        Vc = FSZV * pop_V * exp(pk_η[2])
+        tabs = pop_tabs * exp(pk_η[3])
+        Ka = log(2) / tabs
+        # PD
+        e0 = pop_e0 * exp(pd_η[1])
+        emax = pop_emax * exp(pd_η[2])
+        c50 = pop_c50 * exp(pd_η[3])
+        tover = pop_tover * exp(pd_η[4])
+        kout = log(2) / tover
+        rin = e0 * kout
+        time = t
+    end
+
+    # @dosecontrol begin
+    #     lags = (Depot = pop_lag * exp(lag_η),)
+    # end
+
+    @init begin
+        Turnover = e0
+    end
+
+    # aliases for use in @dynamics and @derived
+    @vars begin
+        cp := Central / Vc
+        ratein := Ka * Depot
+        pd := 1 + emax * cp / (c50 + cp)
+    end
+
+    @dynamics begin
+        Depot'    = -ratein
+        Central'  =  ratein - CL * cp
+        Turnover' =  rin * pd - kout * Turnover
+    end
+
+    @derived begin
+        conc ~ @. Normal(cp, sqrt((σ_prop * cp)^2 + σ_add^2))
+        pca  ~ @. Normal(Turnover, σ_fx)
+    end
+end
+
+bayes_fpm = fit(
+    bayes_model,
     pop,
-    init_params(model),
+    init_params(bayes_model),
     BayesMCMC(
-        nsamples = 1000,
-        nadapts = 500,
+        nsamples = 500,
+        nadapts = 250,
         nchains = 4,
+        # remove the following option in a real run!
+        alg = GeneralizedNUTS(max_depth = 3),
+        diffeq_options = (; alg = Pumas.Rodas5())
     ),
+);
+bayes_fpm_samples = Pumas.discard(bayes_fpm, burnin = 250)
+mean(bayes_fpm_samples)
+coef(fpm)
+
+density_plot(bayes_fpm_samples, parameters = [:pop_tover])
+
+## Post-processing, AUC and Cmax ##
+
+# Get AUC and NCA given simulations
+nca_params = postprocess(reduce(vcat, sims2)) do gen, obs
+    pk_auc = NCA.auc(gen.conc, gen.time)
+    pk_cmax = NCA.cmax(gen.conc, gen.time)
+    pd_auc = NCA.auc(gen.pca, gen.time)
+    pd_cmax = NCA.cmax(gen.pca, gen.time)
+    (; pk_auc, pk_cmax, pd_auc, pd_cmax)
+end
+# Estimate the probability of being in a therapeutic range
+# Can choose the dose to maximize this probability
+prob = mean(nca_params) do p
+    !ismissing(p.pk_auc) && 500.0 < p.pk_auc < 1000.0 && p.pk_cmax <= 15
+end
+
+getproperty.(nca_params, :pk_auc) |> skipmissing |> collect |> hist
+getproperty.(nca_params, :pk_cmax) |> skipmissing |> collect |> hist
+getproperty.(nca_params, :pd_auc) |> skipmissing |> collect |> hist
+getproperty.(nca_params, :pd_cmax) |> skipmissing |> collect |> hist
+
+# Counter-factual simulation
+# New dose simulation for an existing subject
+function new_dose_sim(bayes_fpm_samples, i, nsim, dose_multiplier, obstimes = 0.0:1.0:150.0)
+    ids = unique(df2.ID)
+    subj_df = @rsubset df2 :ID == ids[i]
+    subj_df[1, :AMOUNT] = subj_df[1, :AMOUNT] * dose_multiplier
+    subj_df
+    df2
+    new_subj = read_pumas(
+        subj_df;
+        id = :ID,
+        time = :TIME,
+        amt = :AMOUNT,
+        cmt = :CMT,
+        evid = :EVID,
+        covariates = [:SEX, :WEIGHT, :FSZV, :FSZCL],
+        observations = [:conc, :pca],
+    )[1]
+
+    # Samples the populaton parameters from the posterior
+    # Samples the random effects from the posterior of subject i
+    # Uses the dose and covariates in `new_subj`
+    sims = simobs(
+        bayes_fpm_samples,
+        new_subj;
+        subject = i,
+        samples = nsim,
+        obstimes = obstimes,
+    )
+    bayes_nca_params = postprocess(reduce(vcat, sims)) do gen, obs
+        pk_auc = NCA.auc(gen.conc, gen.time)
+        pk_cmax = NCA.cmax(gen.conc, gen.time)
+        pd_auc = NCA.auc(gen.pca, gen.time)
+        pd_cmax = NCA.cmax(gen.pca, gen.time)
+        (; pk_auc, pk_cmax, pd_auc, pd_cmax)
+    end
+    prob = mean(bayes_nca_params) do p
+        # define desired therapeutic ranges or effects
+        # the following numbers are made up!
+        !ismissing(p.pk_auc) && 1000.0 < p.pk_auc < 2000.0 && p.pk_cmax <= 50
+    end
+    return sims, bayes_nca_params, prob
+end
+
+sims5, bayes_nca_params, prob = new_dose_sim(
+    bayes_fpm_samples, 1, 100, 0.6,
 )
+prob
+
+# Simulate a new subject altogether
+# Samples the population parameters from the posterior
+# Samples the random effects from the prior
+sims6 = simobs(
+    bayes_fpm_samples,
+    # assume this is a new subject
+    pop[1];
+    samples = 100,
+)
+
+# Calculate probability of successful treatment for different doses
+probs = map(0.1:0.1:3.0) do dose_multiplier
+    new_dose_sim(bayes_fpm_samples, 1, 500, dose_multiplier)[3]
+end
+data((dose_multiplier = 0.1:0.1:3.0, probs = probs)) * 
+    mapping(:dose_multiplier => "Dose multiple", :probs => "Probability of successful treatment") * 
+    AOG.visual(Lines) |> draw
