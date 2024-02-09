50a7e7c4747cd1e77c9330844250400f4deefd92
mohamed@pumas.ai <mohamed@pumas.ai>
2024-02-09 16:44:22 +0000

    update tgd-os example

--- tgd-os/tgd-os.jl
+++ tgd-os/tgd-os.jl
@@ -1,41 +1,51 @@
-using Pumas, CSV
+using Pumas, CSV, CairoMakie
 
-# Ref: https://www.sciencedirect.com/science/article/pii/S2451865421001289
+# change the working directory
+if isdir("PAGANZ2024")
+    cd("PAGANZ2024")
+end
+# Current working directory
+pwd()
 
-df = CSV.read("tgd-os/tgd-os-data.csv", DataFrame; missingstring=".")
+df = CSV.read("tgd-os/tgd-os.csv", DataFrame; missingstring=[".", ""])
+tgd_os_pop = read_pumas(
+    df,
+    observations = [:SLD, :Death],
+    covariates = [:WT, :AGE, :SEX],
+    event_data = false,
+)
 
 tgd_os_model = @model begin
-    @params begin
+    @param begin
         # Typical values of the coefficients of the predictors of SLD
-        β ∈ VectorDomain(3)
+        β ∈ VectorDomain(2, init = [0.01, 0.01])
         # IIV of the coefficients of the predictors of SLD
-        Ω ∈ PSDDomain(2)
+        Ω ∈ PSDDomain(init = [0.1 0.0; 0.0 0.1])
         # SLD residual error
-        σ ∈ RealDomain(lower = 0.0)
+        σ ∈ RealDomain(lower = 0.0, init = 1.0)
 
         # Log logistic hazard function parameters
-        h0 ∈ RealDomain(lower = 0.0)
-        κ ∈ RealDomain(; lower=0.0)
+        h0 ∈ RealDomain(lower = 0.0, init = 0.001)
+        κ ∈ RealDomain(; lower=0.0, init = 0.01)
         # Effect of SLD on hazard
-        α ∈ RealDomain()
+        α ∈ RealDomain(init = 0.001)
         # Coefficients of all the other predictors of hazard
         γ ∈ VectorDomain(2)
     end
     @random begin
-        b ~ MvNormal(Ω)
+        η ~ MvNormal(Ω)
     end
     @covariates begin
-        NILSN # number of lesions
         AGE
         SEX
     end
     @pre begin
         # mean SLD
-        m = dot(β, [1, t, NILSN]) + dot(b, [1, t])
-        # 
-        h = h0 * exp(dot(γ, [AGE, SEX]) + α * m)
-        # log logistic hazard instanenous hazard
-        λ := h * κ * (h * t + 1e-10)^(κ - 1) / (1 + (h * t + 1e-10)^κ)
+        m = (β[1] + η[1]) + (β[2] + η[2]) * t
+        # log logistic instanenous hazard
+        sexn = SEX == "Female" ? 0.0 : 1.0
+        h = h0 * exp(γ[1] * AGE + γ[2] * sexn + α * m)
+        λ = h * κ * (h * t + 1e-10)^(κ - 1) / (1 + (h * t + 1e-10)^κ)
     end
     @dynamics begin
         # cumulative hazard
@@ -43,7 +53,23 @@ tgd_os_model = @model begin
     end
     @derived begin
         SLD ~ @. Normal(m, σ)
-        Death ~ TimeToEvent(λ, Λ)
+        Death ~ @. TimeToEvent(λ, Λ)
     end
 end
 
+nsubj = round(Int, 0.8 * length(tgd_os_pop))
+loglikelihood(tgd_os_model, tgd_os_pop[1:nsubj], init_params(tgd_os_model), LaplaceI())
+
+fpm = fit(
+  tgd_os_model,
+  tgd_os_pop[1:nsubj],
+  init_params(tgd_os_model),
+  LaplaceI(),
+)
+
+validation_ll = loglikelihood(
+  tgd_os_model,
+  tgd_os_pop[nsubj+1:end],
+  coef(fpm),
+  LaplaceI(),
+)
